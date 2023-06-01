6a09c6c070a43338e2d9348314ae7c1cf865e234
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 07:34:05 +0200

    Update AppController.java

--- src/main/java/com/paclt/Bank/app/controller/AppController.java
+++ src/main/java/com/paclt/Bank/app/controller/AppController.java
@@ -22,22 +22,22 @@ import java.util.List;
 @Controller
 public class AppController {
     public static int sum(int a, int b) {
-        return a + b;
-    }
+    return a + b;
+}
 
-    public static void bubbleSort(int[] array) {
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    // Swap array[j] and array[j+1]
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
+public static void bubbleSort(int[] array) {
+    int n = array.length;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap array[j] and array[j+1]
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
+}
 
     private final CustomUserDetailsServiceImpl customUserDetailsService;
 
