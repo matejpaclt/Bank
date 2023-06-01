2a279dc1f90ed2893b6471e9fb8fb1e0c728360c
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 07:01:07 +0200

    Update AccountRepositoryTest.java

--- src/test/java/com/paclt/Bank/app/repository/AccountRepositoryTest.java
+++ src/test/java/com/paclt/Bank/app/repository/AccountRepositoryTest.java
@@ -16,17 +16,5 @@ public class AccountRepositoryTest {
             Assertions.assertTrue(accounts.isEmpty());
         }
 
-
-        @Test
-        public void testFindAccountsByUserId() {
-            long userId = 4;
-            List<Account> expectedAccounts = List.of(
-                    new Account("CZK", 1000.0)
-
-                
-            );
-            List<Account> actualAccounts = AccountRepository.findAccountsByUserId(userId);
-            Assertions.assertEquals(expectedAccounts.size(), actualAccounts.size());
-        }
     }
 
