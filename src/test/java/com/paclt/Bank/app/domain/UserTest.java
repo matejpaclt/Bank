34c4f68ad44526c1aee578546a5c6d86c5b5e2c2
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 05:53:52 +0200

    Update UserTest.java

--- src/test/java/com/paclt/Bank/app/domain/UserTest.java
+++ src/test/java/com/paclt/Bank/app/domain/UserTest.java
@@ -54,7 +54,7 @@ public class UserTest {
 
         // Verify the account details
         assertEquals("USD", accounts.get(0).getName());
-        assertEquals(1000.0, accounts.get(0).getBalance());
+        assertEquals(150.0, accounts.get(0).getBalance());
         assertEquals("USD", accounts.get(1).getName());
         assertEquals(500.0, accounts.get(1).getBalance());
         assertEquals("EUR", accounts.get(2).getName());
