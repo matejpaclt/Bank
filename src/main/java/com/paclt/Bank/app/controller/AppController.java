af82cea6617f570f2414154bc8b1e109517a54af
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 07:15:20 +0200

    Update AppController.java

--- src/main/java/com/paclt/Bank/app/controller/AppController.java
+++ src/main/java/com/paclt/Bank/app/controller/AppController.java
@@ -194,5 +194,22 @@ public class AppController {
         // Call handlePayment method indirectly
         return handlePayment(amount, model, accountType, authentication);
     }
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
 }
 
