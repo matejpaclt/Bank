536f76438fe2af26bd41f327969c4f760ab5fab2
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 06:07:36 +0200

    Update AccountRepositoryTest.java

--- src/test/java/com/paclt/Bank/app/repository/AccountRepositoryTest.java
+++ src/test/java/com/paclt/Bank/app/repository/AccountRepositoryTest.java
@@ -8,22 +8,23 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class AccountRepositoryTest {
-
-    @Test
-    public void testFindAccountsByUserId_ReturnsEmptyListWhenFileNotFound() {
-        List<Account> accounts = AccountRepository.findAccountsByUserId(999);
-        Assertions.assertTrue(accounts.isEmpty());
-    }
-
     
-    @Test
-    public void testFindAccountsByUserId() {
-        long userId = 4;
-        List<Account> expectedAccounts = List.of(
-                new Account("CZK", 1000.0)
-        );
-        List<Account> actualAccounts = AccountRepository.findAccountsByUserId(userId);
-        Assertions.assertEquals(expectedAccounts.size(), actualAccounts.size());
+
+        @Test
+        public void testFindAccountsByUserId_ReturnsEmptyListWhenFileNotFound() {
+            List<Account> accounts = AccountRepository.findAccountsByUserId(999);
+            Assertions.assertTrue(accounts.isEmpty());
+        }
+
+
+        @Test
+        public void testFindAccountsByUserId() {
+            long userId = 4;
+            List<Account> expectedAccounts = List.of(
+                    new Account("CZK", 1000.0)
+            );
+            List<Account> actualAccounts = AccountRepository.findAccountsByUserId(userId);
+            Assertions.assertEquals(expectedAccounts.size(), actualAccounts.size());
+        }
     }
-}
 
