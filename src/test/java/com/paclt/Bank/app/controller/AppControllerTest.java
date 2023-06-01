039b9e6a249ec00227a40a8bbd9b949259f2dfe8
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2023-06-01 07:54:59 +0200

    Update AppControllerTest.java

--- src/test/java/com/paclt/Bank/app/controller/AppControllerTest.java
+++ src/test/java/com/paclt/Bank/app/controller/AppControllerTest.java
@@ -8,7 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
-
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
